3328d9c0e7ad267d2f076f9cc893423920b8d7be
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2022-09-12 20:18:27 -0300

    add new page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
 import { Routes, Route, Link } from "react-router-dom";
 import styled from "styled-components";
-import { withStepProgress, useStepProgress, Step, StepProgressBar} from 'react-stepz'
+import StepProgressBar from 'react-step-progress';
+import 'react-step-progress/dist/index.css';
+
 
 const NavUnlisted = styled.ul`
   text-decoration: none;
@@ -104,10 +106,52 @@ function Scan(){
 }
 
 function Quiz(){
-  
+  // setup the step content
+const step1Content = <h1>Step 1 Content</h1>;
+const step2Content = <h1>Step 2 Content</h1>;
+const step3Content = <h1>Step 3 Content</h1>;
+
+// setup step validators, will be called before proceeding to the next step
+function step2Validator() {
+  // return a boolean
+}
+
+function step3Validator() {
+  // return a boolean
+}
+
+function onFormSubmit() {
+  // handle the submit logic here
+  // This function will be executed at the last step
+  // when the submit button (next button in the previous steps) is pressed
+}
+
 
   return <>
-  
+    <StepProgressBar
+  startingStep={0}
+  onSubmit={onFormSubmit}
+  steps={[
+    {
+      label: 'Step 1',
+      subtitle: '10%',
+      name: 'step 1',
+      content: step1Content
+    },
+    {
+      label: 'Step 2',
+      subtitle: '50%',
+      name: 'step 2',
+      content: step2Content,
+    },
+    {
+      label: 'Step 3',
+      subtitle: '100%',
+      name: 'step 3',
+      content: step3Content,
+    }
+  ]}
+/>;
   </>
 }
 export default App
